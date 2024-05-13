975b4e82e03ac4ddcfc3e74c61722eeb5d8e88a7
Ryan Conk <ryangconk@gmail.com>
2024-05-13 23:15:53 +0000

    add the projkec

---
+++ src/components/Map/index.js
@@ -0,0 +1,15 @@
+import './styles.css'
+import MapChart from './Map'
+import React from 'react'
+
+const Map = ({ isChecked }) => {
+  return (
+    <>
+      <div className="map-border">
+        <MapChart isChecked={isChecked} />
+      </div>
+    </>
+  )
+}
+
+export default Map
